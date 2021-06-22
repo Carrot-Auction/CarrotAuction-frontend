bbc699eed17647bd034d4816c6f96ec1138c7799
Bony <gubonhy@naver.com>
2021-06-23 03:34:32 +0900

    api connect

--- src/pages/ItemDetailPage.tsx
+++ src/pages/ItemDetailPage.tsx
@@ -94,7 +94,9 @@ export const ItemDetailPage: React.FC = () => {
             borderWidth="0.1rem"
             marginTop="1.5em"
           ></Box>
+          <Box>
           {item ? <ItemDetail {...item}></ItemDetail> : <Box>로딩중...</Box>}
+          <Box>
           <Box fontWeight="600" fontSize="1rem" marginTop="2rem">
             이 상품과 함께 봤어요
           </Box>
